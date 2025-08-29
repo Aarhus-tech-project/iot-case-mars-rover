0ac3af7e4cee945329caed586985ac7ccb2aab53
Markus <mssbabou@gmail.com>
2025-08-29 08:46:36 +0200

    Refactor lidar processing to utilize backbuffer and improve grid updates

--- RoverCore/src/main.cpp
+++ RoverCore/src/main.cpp
@@ -70,26 +70,20 @@ int main() {
     }
     std::fprintf(stderr, "[lidar] reading %s @%d (max=%dmm)\n", LIDAR_SERIAL_PORT, LIDAR_SERIAL_BAUD, LIDAR_MAX_MM);
 
+    std::vector<Ray> buffer;
+    std::vector<Ray> backBuffer;
+    uint32_t last_angle_cdeg;
     auto cb = [&](uint32_t angle_cdeg, uint32_t dist_mm, uint32_t intensity, uint64_t t_ns) {
-        Ray ray(rover_x_m, rover_y_m, angle_cdeg / 100.0f, dist_mm, t_ns);
-
-        auto [gx, gy] = grid.worldToGrid(ray.point_x_m, ray.point_y_m);
-        if (grid.inBounds(gx, gy)) grid.at(gx, gy) += 80;
-
-        auto [rgx, rgy] = grid.worldToGrid(rover_x_m, rover_y_m);
-        int dx = std::abs(gx - rgx), sx = rgx < gx ? 1 : -1;
-        int dy = -std::abs(gy - rgy), sy = rgy < gy ? 1 : -1;
-        int err = dx + dy, e2;
-        while (true) {
-            if (grid.inBounds(rgx, rgy)) {
-                uint8_t& v = grid.at(rgx, rgy);
-                if (v > 0) v -= 20;
-            }
-            if (rgx == gx && rgy == gy) break;
-            e2 = 2 * err;
-            if (e2 >= dy) { err += dy; rgx += sx; }
-            if (e2 <= dx) { err += dx; rgy += sy; }
+        if (angle_cdeg < last_angle_cdeg) {
+            // bring backbuffer to frontbuffer
+            // Clean backbuffer
+            buffer = std::move(backBuffer);
         }
+
+        Ray ray(rover_x_m, rover_y_m, angle_cdeg / 100.0f, dist_mm, t_ns);
+        backBuffer.push_back(ray);
+
+        last_angle_cdeg = angle_cdeg;
     };
 
     auto last_push = std::chrono::steady_clock::now();
@@ -98,6 +92,27 @@ int main() {
 
         auto now = std::chrono::steady_clock::now();
         if (now - last_push >= std::chrono::seconds(5)) {
+            
+            std::cout << "Buffer size: " << buffer.size() << std::endl;
+            for (Ray& ray : buffer) {
+                auto [gx, gy] = grid.worldToGrid(ray.point_x_m, ray.point_y_m);
+                if (grid.inBounds(gx, gy)) grid.at(gx, gy) += 80;
+
+                auto [rgx, rgy] = grid.worldToGrid(rover_x_m, rover_y_m);
+                int dx = std::abs(gx - rgx), sx = rgx < gx ? 1 : -1;
+                int dy = -std::abs(gy - rgy), sy = rgy < gy ? 1 : -1;
+                int err = dx + dy, e2;
+                while (true) {
+                    if (grid.inBounds(rgx, rgy)) {
+                        uint8_t& v = grid.at(rgx, rgy);
+                        if (v > 0) v -= 20;
+                    }
+                    if (rgx == gx && rgy == gy) break;
+                    e2 = 2 * err;
+                    if (e2 >= dy) { err += dy; rgx += sx; }
+                    if (e2 <= dx) { err += dx; rgy += sy; }
+                }
+            }
 
             // Push telemetry over GRPC
             GridFrame gridFrame;
