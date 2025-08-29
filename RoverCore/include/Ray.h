83be88c04b1fe4fc55c44e314269dce564cb6616
Markus <mssbabou@gmail.com>
2025-08-29 09:14:44 +0200

    Get ray population code into occupancygrid struct

--- RoverCore/include/Ray.h
+++ RoverCore/include/Ray.h
@@ -6,8 +6,8 @@ class Ray {
 public:
     float origin_x_m;
     float origin_y_m;
-
     float angle_deg;
+
     float direction_x;
     float direction_y;
 
@@ -29,7 +29,6 @@ inline Ray::Ray(float _origin_x_m, float _origin_y_m, float _angle_deg, float _d
     distance_mm = _distance_mm;
     time_ns     = _time_ns;
 
-    // your original math: theta_math = 90 - lidar_angle
     float r_m = _distance_mm * 0.001f;
     float theta_math_deg = 90.0f - _angle_deg;
     float rad = theta_math_deg * float(M_PI) / 180.0f;
