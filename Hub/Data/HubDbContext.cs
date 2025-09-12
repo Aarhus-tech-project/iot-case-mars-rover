5d1fa2d20330f1299c34369b4e2acc07e3733624
Jacob9691 <48980223+Jacob9691@users.noreply.github.com>
2025-09-12 10:25:28 +0200

    added db connection with EntityFramework migration

---
+++ Hub/Data/HubDbContext.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Hub.Models;
+
+namespace Hub.Data
+{
+    public class HubDbContext : DbContext
+    {
+        public DbSet<CommandMessage> CommandMessages { get; set; }
+        public DbSet<CommandStatus> CommandStatuses { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<CommandStatus>().HasData(
+                new CommandStatus { Id = 1, Description = "Pending" },
+                new CommandStatus { Id = 2, Description = "Sent" },
+                new CommandStatus { Id = 3, Description = "Received" },
+                new CommandStatus { Id = 4, Description = "Failed" }
+            );
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // read from config
+                var config = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                    .AddJsonFile($"appsettings.Development.json", optional: true, reloadOnChange: true)   // optional for local dev
+                    .AddEnvironmentVariables()
+                    .Build();
+
+                var connectionString = config.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+        }
+    }
+}
---
+++ Hub/Models/CommandMessage.cs
@@ -0,0 +1,13 @@
+namespace Hub.Models
+{
+    public class CommandMessage
+    {
+        public int Id { get; set; }
+        public string? CommandText { get; set; }      // Command sent from website
+        public string? ReplyText { get; set; }        // Response from Rover
+        public int StatusId { get; set; }              // Foreign key to CommandStatus
+        public required CommandStatus Status { get; set; }      // Pending, Sent, Received, Failed
+        public DateTime CommandSentAt { get; set; } = DateTime.UtcNow;
+        public DateTime? ReplyReceivedAt { get; set; } // Nullable, filled when reply comes
+    }
+}
